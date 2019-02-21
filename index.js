273a19e60cced95608343f9d06239771fa594a3d
Dusty211 <kylebenz@hotmail.com>
2019-02-21 09:16:02 -0500

    all passing.

--- index.js
+++ index.js
@@ -11,5 +11,23 @@ const logDriversByHometown = function(drivers, hometown) {
 }
 
 const driversByRevenue = function(drivers) {
-  
+  let driversClone = [...drivers]
+  driversClone.sort( function(a, b) {return a.revenue - b.revenue})
+  return driversClone
+}
+
+const driversByName = function(drivers) {
+  let driversClone = [...drivers]
+  driversClone.sort( function(a, b) {return a.name.localeCompare(b.name)})
+  return driversClone
+}
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue; //thank you MDN
+
+const totalRevenue = function(drivers) {
+  return drivers.map(driver => driver.revenue).reduce(reducer)
+}
+
+const averageRevenue = function(drivers) {
+  return totalRevenue(drivers) / drivers.length
 }
