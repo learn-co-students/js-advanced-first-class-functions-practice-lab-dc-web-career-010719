f1fa41b669c3431b4f72efd426f4ce1d3e8b641e
Dusty211 <kylebenz@hotmail.com>
2019-02-21 08:24:24 -0500

    2 functions working

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+
+const logDriverNames = function(drivers) {
+  drivers.forEach( function(driver) {
+    console.log(driver.name)
+  })
+}
+
+const logDriversByHometown = function(drivers, hometown) {
+  logDriverNames(drivers.filter(driver => driver.hometown === hometown))
+}
+
+const driversByRevenue = function(drivers) {
+  
+}
